d73bd040a9f25d895ed1ba43f02ca09408e26228
Artur Guimaraes <arturguimaraes92@hotmail.com>
2017-03-23 13:38:11 -0300

    Map ready with search box

--- application/views/pages/inform.php
+++ application/views/pages/inform.php
@@ -20,81 +20,59 @@
                     <a href="<?=$baseURL?>">Home</a>
                 </li>
                 <li class="page-scroll menu-item">
-                    <a href="#data">Informe os Dados</a>
+                    <a href="#mapPos">Mapa</a>
                 </li>
                 <li class="page-scroll menu-item">
-                    <a href="#map">Mapa</a>
+                    <a href="#data">Informe os Dados</a>
                 </li>
             </ul>
         </div>
     </div>
 </nav>
 
-<!-- DATA -->
-<section id="data" class="container content-section text-center">
-	<div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2 margin-bottom-50">
-            	<div class="col-md-2"><img class="rotate" src="assets/img/icon/mail.png"></div>
-                <div class="col-md-10"><h2>Entre em contato!</h2></div>
-                <form role="form" action="" method="post">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="name">Seu Nome</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control" name="name" id="name" placeholder="Digite o seu nome" required="">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Seu E-mail</label>
-                            <div class="input-group">
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail" required="">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="message">Mensagem</label>
-                            <div class="input-group">
-                                <textarea name="message" id="message" class="form-control" rows="5" required></textarea>
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok form-control-feedback"></i></span>
-                            </div>
-                        </div>
-                        <input type="submit" name="submit" id="submit" value="Submit" class="btn wow tada btn-embossed btn-primary pull-right animated animated" style="visibility: visible;">
-                    </div>
-                </form>
-                <hr class="featurette-divider hidden-lg">
-                    <div class="col-md-5 col-md-push-1 address">
-                        <address>
-                        <h3>Onde estamos</h3>
-                        <p class="lead"><a href="https://www.google.com.br/maps/place/R.+do+Ouvidor,+90+-+Centro,+Rio+de+Janeiro+-+RJ,+20040-030/@-22.9031712,-43.1795134,17z/data=!3m1!4b1!4m5!3m4!1s0x997f5ee3767063:0x841942c7bca59ebb!8m2!3d-22.9031712!4d-43.1773247">Rua do Ouvidor, 90<br>
-                        Rio de Janeiro, RJ 20040-030</a>
-                        Telefone: +55 21 3190 1971</p>
-                        </address>
-    
-                        <h3>Social</h3>
-                        <li class="social"> 
-                        <a href="#"><i class="fa fa-facebook-square fa-size"> </i></a>
-                        <a href="#"><i class="fa  fa-twitter-square fa-size"> </i> </a> 
-                        <a href="#"><i class="fa fa-google-plus-square fa-size"> </i></a>
-                        <a href="#"><i class="fa fa-flickr fa-size"> </i> </a>
-                        </li>
-                    </div>
-                </div>
+<!-- MAP -->
+<section id="mapPos" class="container content-section text-center">
+    <div class="row">
+        <div class="col-md-12 margin-bottom-20">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="col-md-2"><img class="rotate" src="assets/img/icon/map.png"></div>
+                <div class="col-md-10"><h2>Encontre o local</h2></div>
+            </div>
+            <div class="col-md-12 mapDiv margin-top-20">
+                <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+                <div id="map"></div> 
+                <!-- Map Javascript Function -->
+                <script src="assets/js/map.js"></script>    
+                <!-- Google Maps API -->
+                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx7RRomppFzBUCQfToBi22kF2UsdEU6Iw&libraries=places&callback=initAutocomplete"></script>
             </div>
         </div>
-	</div>
+    </div>
 </section>
 
-<!-- MAP -->
-<section id="map" class="content-section text-center">
-    <div class="">
-        <div class="col-md-12">
-            <div id="googleMap" style="width:100%;height:400px;"></div> 
-            <!-- Map Javascript Function -->
-    		<script src="assets/js/map.js"></script>    
-            <!-- Google Maps API -->
-			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx7RRomppFzBUCQfToBi22kF2UsdEU6Iw&callback=loadMap"></script>
-        </div>
-    </div>
+<!-- data -->
+<section id="data" class="container content-section text-center">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2 margin-bottom-20">
+        	<div class="col-md-2"><img class="rotate" src="assets/img/icon/clipboard.png"></div>
+            <div class="col-md-10 margin-bottom-50"><h2>Informe os dados</h2></div>
+            <form id="createForm" class="form-horizontal col-md-12" method="post">
+              <fieldset>
+                <div class="form-group col-md-5">
+                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" autofocus>
+                    <br><input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Sobrenome">
+                    
+                </div> 
+                <div class="form-group col-md-2"></div> 
+                <div class="form-group col-md-5">
+                    <input type="text" class="form-control" id="CPF" name="CPF" placeholder="CPF (xxx.xxx.xxx-xx)" maxlength="14">
+                    <br><input type="text" class="form-control" id="dataNasc" name="dataNasc" placeholder="Data de Nascimento (dd/mm/aaaa)" maxlength="10">
+                </div>
+                <div class="form-group col-md-12">
+                    <button type="submit" class="btn btn-success" name="submit">Enviar</button>
+                </div>    
+              </fieldset>
+            </form>
+		</div>
+	</div>
 </section>
